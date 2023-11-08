b69554c86fd997594c8f3bee6dfa5df260afbdda
Xavier Garzon <xavier.garzon@getitllc.com>
2023-11-07 23:29:28 -0500

    fix script

--- install-docker.sh
+++ install-docker.sh
@@ -1,5 +1,5 @@
 # Install packages to allow apt to use a repository over HTTPS
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 
 # Add Docker’s official GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
